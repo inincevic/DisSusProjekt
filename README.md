ed83c2d34b3024042ecf1819ec5f3fed9ac3462d
inincevic <inincevic@unipu.hr>
2024-03-01 17:16:09 +0100

    Created a function for contacting workers

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # DisSusProjekt
 Repozitorij u kojemu ce biti moj projekt za kolegij Raspodijeljeni sustavi
 
+
+## Rad aplikacije
+Kako bi postojao smisao load balancera, potrebno je odrediti sto ce workeri aplikacije raditi.
+Kako bi se smanjilo kompliciranje i kolicina rada provedenog na workerima, workeri ce, barem za sada biti zauzeti i cekati 20s prije davanja odgovora i oslobadjanja.
+
 ## Pokretanje load balancera
 Unutar ./src/ potrebno je pokreniti slijedecu naredbu
 > python -m uvicorn balancer:app --reload
--- src/balancer.py
+++ src/balancer.py
@@ -1,8 +1,24 @@
 import fastapi
 
 app = fastapi.FastAPI()
+import httpx
+import json
 
 # Test route
 @app.get("/")
 def test_get():
     return "The code works."
+
+# Function which connects to a different code
+async def contact_worker(port, message):
+    port_str = str(port)
+    url = "http://127.0.0.1:" + port_str + "/fib/" + message
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        return json.loads(response.text)
+
+
+# Ruta which sends tasks to the worker
+@app.get("/send_work")
+def send_work():
+    pass
