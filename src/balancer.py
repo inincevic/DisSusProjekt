506541f7b6a960f715dd1f63d5c0db695eb1d758
inincevic <inincevic@unipu.hr>
2024-03-06 17:20:35 +0100

    Enabled registering workers to the balancer as well as created a route that enables us to see which workers are registered.

--- src/balancer.py
+++ src/balancer.py
@@ -4,10 +4,35 @@ app = fastapi.FastAPI()
 import httpx
 import json
 
+# List of all available workers
+workers = [] 
+
 # Test route
 @app.get("/")
 def test_get():
     return "The code works."
+
+# Route for checking all available servers
+@app.get("/available_workers")
+def available():
+    return workers
+
+# Route on which the workers register in the load balancer
+@app.get("/register_worker/{port}")
+def new_worker(request: fastapi.Request, port):
+    client_ip = request.client.host
+    client_port = port
+
+    print(f"Request from {client_ip}:{client_port}")
+
+    worker_name = "worker" + str(len(workers))
+    workers.append({
+        "worker": worker_name,
+        "port": str(client_port),
+        "running": 0,
+        "total_tasks_done": 0
+    })
+    return 201
 
 # Function which connects to a different code
 async def contact_worker(port, message):
--- src/worker.py
+++ src/worker.py
@@ -1,7 +1,17 @@
-import fastapi, time
+import fastapi, time, httpx, sys, json
 
 app = fastapi.FastAPI()
 
+# Startup event that contacts the balancer and registers the worker, with the port it is running on
+@app.on_event("startup")
+async def contact_server():
+    async with httpx.AsyncClient() as client:
+        if "--port" in sys.argv:
+            index = sys.argv.index("--port")
+            port = int(sys.argv[index + 1])
+        print(port)
+        response = await client.get("http://127.0.0.1:8000/register_worker/" + str(port))
+        return json.loads(response.text)
 
 # Test route
 @app.get("/")
