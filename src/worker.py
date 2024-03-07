a7ed7838e67c736940bdd2826be52662599e3f1b
inincevic <inincevic@unipu.hr>
2024-03-07 15:55:57 +0100

    Enabled checking if the message was added to the file or not

--- src/worker.py
+++ src/worker.py
@@ -42,25 +42,43 @@ def do_work(message):
     print(f"Done sleeping, returning reversed message: {message2}")
     return message2
 
-# Part of code that will be the "core" of the worker.
-# Messages given from the user to the balancer will be forwarded to the worker, where they will be
-# written into the file. The data from this code can also be read out.
-# These operations take some time, just enough for the simulation of the system's work.
-# All workers will write into the same file, simulating a system communicating with a database.
+
+'''
+Part of code that will be the "core" of the worker.
+Messages given from the user to the balancer will be forwarded to the worker, where they will be
+written into the file. The data from this code can also be read out.
+These operations take some time, just enough for the simulation of the system's work.
+All workers will write into the same file, simulating a system communicating with a database.
+'''
+# Part of code that writes to file
 @app.get("/write_to_file/{message}")
-def write_to_file(message):
-    write_file_name_start_lenght = len(write_file_name)
+async def write_to_file(message):
+    # Establishing the number of lines in the file, so that writing into the file can be confirmed.
+    with open(write_file_name, "r") as file:
+        lines = file.readlines()
+        write_file_name_start_lenght = len(lines)
+
+    # Adding a line break to the end of the message so that messages can be separated from each other.
     message_to_write = message + "\n"
+
+    # Writing the message into the file
     with open(write_file_name, "a") as file:
             file.write(message_to_write)
-    if(len(write_file_name) > write_file_name_start_lenght):
+    
+    # Checking the current number of lines in the file.
+    with open(write_file_name, "r") as file:
+        lines = file.readlines()
+        write_file_name_end_lenght = len(lines)
+
+    # Checking if the message was added and returning an apropriate reply.
+    if(write_file_name_end_lenght > write_file_name_start_lenght):
         return "The message has been written into the file."
     else:
         return "An error ocurred while writing into file."
 
+# Part of code that reads from file
 @app.get("/read_from_file")
 def read_from_file():
     with open(write_file_name, "r") as file:
         lines = file.readlines()
     return lines
-    
